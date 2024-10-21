8ce98a06691322e1b85beb302cb91bea5257bd93
Diego <dportero@montran.com>
2024-10-21 10:41:26 -0500

    Change button color and add footer

--- src/components/HalloweenPumpkinApp.tsx
+++ src/components/HalloweenPumpkinApp.tsx
@@ -117,7 +117,7 @@ function HalloweenPumpkinApp() {
                 />
                 <button
                     onClick={clearCanvas}
-                    className="bg-red-500 text-white px-4 py-2 rounded flex items-center"
+                    className="bg-[#f97316] text-white px-4 py-2 rounded flex items-center"
                 >
                     <Eraser className="mr-2" />
                     Borrar
--- src/components/TablaPosiciones.tsx
+++ src/components/TablaPosiciones.tsx
@@ -50,7 +50,7 @@ function TableApp() {
                                 })
 
                             }}
-                            className="bg-red-500 text-white px-4 py-2 rounded flex items-center mx-auto">
+                            className="bg-[#f97316] text-white px-4 py-2 rounded flex items-center mx-auto">
                             <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-notes mr-2">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" />
