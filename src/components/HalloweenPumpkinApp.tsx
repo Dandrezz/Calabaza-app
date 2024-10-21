cdd6f837f6932ec3c180d0ba7b71f116469a20fc
Dandrezz <diego@gmail.com>
2024-10-21 01:52:27 -0500

    first commit

---
+++ src/components/HalloweenPumpkinApp.tsx
@@ -0,0 +1,149 @@
+import { useRef, useState, useEffect } from 'react';
+import { Upload, Eraser } from 'lucide-react';
+
+const URL_API_DATA = 'https://calabaza-api-app.diegoaporterol.workers.dev/api/calabaza'
+const URL_CLOUDINARY = 'https://api.cloudinary.com/v1_1/dqvtr77op/image/upload'
+
+function HalloweenPumpkinApp() {
+    const canvasRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [color, setColor] = useState('#FFA500');
+    const [lineWidth, setLineWidth] = useState(2);
+    const [autor, setAutor] = useState('Calabazin')
+    
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
+    }, [color, lineWidth]);
+
+    const startDrawing = (e) => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        setIsDrawing(true);
+    };
+
+    const draw = (e) => {
+        if (!isDrawing) return;
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    };
+
+    const stopDrawing = () => {
+        const ctx = canvasRef.current.getContext('2d');
+        ctx.closePath();
+        setIsDrawing(false);
+    };
+
+    const handleUpload = async () => {
+        const canvas = canvasRef.current;
+        const imageData = canvas.toDataURL('image/png');
+
+        const data = new FormData()
+        data.append('file', imageData)
+        data.append('upload_preset', 'aqjx77cr')
+
+        fetch(URL_CLOUDINARY, {
+            method: "post",
+            body: data
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data)
+                fetch(URL_API_DATA, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        autor: 'anonimo',
+                        puntos: 0,
+                        url: data.secure_url
+                    })
+                }).then(res=>res.json())
+                .then(data=>
+                    window.location.href = '/tabla'
+                )
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    };
+
+    const clearCanvas = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    };
+
+    return (
+        <div className="flex flex-col items-center p-4">
+            <h1 className="text-2xl font-bold mb-4">Dibuja tu Calabaza de Halloween</h1>
+
+            <span className='text-white mb-4 text-2xl'>Nombre: {autor}</span>
+            <input type="text" value={autor} className='mb-4' onChange={(e)=>{
+                setAutor(e.target.value)
+            }}/>
+
+            <div className="mb-4 flex space-x-4">
+                <input
+                    type="color"
+                    value={color}
+                    onChange={(e) => setColor(e.target.value)}
+                    className="w-10 h-10"
+                />
+                <input
+                    type="range"
+                    min="1"
+                    max="20"
+                    value={lineWidth}
+                    onChange={(e) => setLineWidth(e.target.value)}
+                    className="w-32"
+                />
+                <button
+                    onClick={clearCanvas}
+                    className="bg-red-500 text-white px-4 py-2 rounded flex items-center"
+                >
+                    <Eraser className="mr-2" />
+                    Borrar
+                </button>
+            </div>
+            <canvas
+                ref={canvasRef}
+                width={400}
+                height={400}
+                onMouseDown={startDrawing}
+                onMouseMove={draw}
+                onMouseUp={stopDrawing}
+                onMouseOut={stopDrawing}
+                className="border border-gray-300 mb-4"
+            />
+            <button
+                onClick={handleUpload}
+                className="bg-orange-500 text-white px-4 py-2 rounded flex items-center"
+            >
+                <Upload className="mr-2" />
+                Subir y Mejorar
+            </button>
+
+        </div>
+    );
+}
+
+export default HalloweenPumpkinApp
---
+++ src/components/TablaPosiciones.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+
+const URL_API_DATA = 'https://calabaza-api-app.diegoaporterol.workers.dev/api/calabaza'
+const URL_CLOUDINARY = 'https://api.cloudinary.com/v1_1/dqvtr77op/image/upload'
+
+function TableApp() {
+
+    const [calabazas, setCalabazas] = useState([])
+
+    useEffect(() => {
+
+        fetch(URL_API_DATA)
+            .then(res => res.json())
+            .then(data => {
+                setCalabazas(data)
+            })
+
+    }, [])
+
+
+
+    return (
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
+            {
+                calabazas.map(calabaza => (
+                    <div className="text-center" key={calabaza.id}>
+                        <span className="font-bold text-xl">{calabaza.autor}</span>
+                        <img className="mx-auto" src={calabaza.url} alt="calabaza" />
+                        <div className="mb-4">Puntos: {calabaza.puntos}</div>
+                        <button 
+                            onClick={()=>{
+                                calabaza.puntos += 1
+                                fetch(URL_API_DATA+`/${calabaza.id}`, {
+                                    method: 'PUT',
+                                    headers: {
+                                        'Content-Type': 'application/json',
+                                    },
+                                    body: JSON.stringify({
+                                        ...calabaza,
+                                        puntos: calabaza.puntos
+                                    })
+                                }).then(res=>res.json())
+                                .then(data=>{
+                                    console.log(data)
+                                    setCalabazas(prev=>{
+                                        const prevFiltered = prev.filter((value) => value.id !== calabaza.id);
+                                        console.log([...prevFiltered, calabaza])
+                                        return [...prevFiltered, calabaza];
+                                    })
+                                })
+
+                            }}
+                            className="bg-red-500 text-white px-4 py-2 rounded flex items-center mx-auto">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-notes mr-2">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" />
+                                <path d="M9 7l6 0" />
+                                <path d="M9 11l6 0" />
+                                <path d="M9 15l4 0" />
+                            </svg>
+                            Votar</button>
+                    </div>
+                ))
+            }
+        </div>
+    )
+
+}
+
+export default TableApp;
